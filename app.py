60c7e64555d61ab85d0a8f9334537c44b8e56439
yin895902139 <40667536+yin895902139@users.noreply.github.com>
2023-05-17 13:45:58 +0000

    加上ChatGPT写的文字动画

--- app.py
+++ app.py
@@ -2,5 +2,31 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+# def hello_world():
+#    return '我爱你，小莹！' 
 def hello_world():
-    return '我爱你，小莹！'
+    return '''
+    <html>
+    <head>
+      <style>
+        /* 定义动画效果 */
+        @keyframes text-animation {
+          0% { opacity: 0; }
+          50% { opacity: 1; }
+          100% { opacity: 0; }
+        }
+        
+        /* 应用动画到文字元素 */
+        .animated-text {
+          animation: text-animation 2s infinite;
+        }
+      </style>
+    </head>
+    <body>
+      <h1 class="animated-text">我爱你，小莹！</h1>
+    </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run()
