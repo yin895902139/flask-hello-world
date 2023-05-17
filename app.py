d4941038d6962692facadf066e58e0148b0e09b4
yin895902139 <40667536+yin895902139@users.noreply.github.com>
2023-05-17 14:00:58 +0000

    加入显示当前时间

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-# def hello_world():
-#    return '我爱你，小莹！' 
 def hello_world():
     return '''
     <html>
@@ -20,16 +19,25 @@ def hello_world():
         .animated-text {
           animation: text-animation 2s infinite;
         }
-
+        
         /* 设置桃心字符的文本颜色为红色 */
         .heart {
           color: red;
         }
-
       </style>
+      <script>
+        function updateTime() {
+          var element = document.getElementById("current-time");
+          var currentTime = new Date().toLocaleTimeString();
+          element.innerText = currentTime;
+        }
+        
+        setInterval(updateTime, 1000); // 每秒更新一次时间
+      </script>
     </head>
     <body>
-      <h1 class="animated-text">我爱你，小莹<span class="heart">\u2764</span>！</h1>
+      <h1 class="animated-text">我爱你<span class="heart">❤</span>，小莹！</h1>
+      <p>当前时间：<span id="current-time"></span></p>
     </body>
     </html>
     '''
