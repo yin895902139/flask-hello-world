6b9822b71e613311dc2b2f874ac65a719d83b8b8
yin895902139 <40667536+yin895902139@users.noreply.github.com>
2023-05-17 14:41:12 +0000

    只显示时间

--- app.py
+++ app.py
@@ -1,53 +1,43 @@
 from flask import Flask
-import datetime
 
 app = Flask(__name__)
 
-start_time = datetime.datetime(2023, 5, 20)  # 设置起始时间
-
 @app.route('/')
 def hello_world():
-    current_time = datetime.datetime.now()
-    time_difference = current_time - start_time
-
-    days = time_difference.days
-    minutes, seconds = divmod(time_difference.seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-
-    return f'''
+    return '''
     <html>
     <head>
       <style>
         /* 定义动画效果 */
-        @keyframes text-animation {{
-          0% {{ opacity: 0; }}
-          50% {{ opacity: 1; }}
-          100% {{ opacity: 0; }}
-        }}
+        @keyframes text-animation {
+          0% { opacity: 0; }
+          50% { opacity: 1; }
+          100% { opacity: 0; }
+        }
         
         /* 应用动画到文字元素 */
-        .animated-text {{
+        .animated-text {
           animation: text-animation 2s infinite;
-        }}
+        }
         
         /* 设置桃心字符的文本颜色为红色 */
-        .heart {{
+        .heart {
           color: red;
-        }}
+        }
       </style>
       <script>
-        function updateTime() {{
-          var element = document.getElementById("time-together");
-          var timeString = "{days}天 {hours}小时 {minutes}分钟 {seconds}秒";
-          element.innerHTML = "我们在一起已经 " + timeString;
-        }}
+        function updateTime() {
+          var element = document.getElementById("current-time");
+          var currentTime = new Date().toLocaleTimeString();
+          element.innerText = currentTime;
+        }
         
-        window.setInterval(updateTime, 1000); // 每秒更新一次时间
+        setInterval(updateTime, 1000); // 每秒更新一次时间
       </script>
     </head>
-    <body onload="updateTime()"> <!-- 使用 onload 事件调用 updateTime 函数 -->
+    <body>
       <h1 class="animated-text">我爱你<span class="heart">❤</span>，小莹！</h1>
-      <p id="time-together"></p>
+      <p>当前时间：<span id="current-time"></span></p>
     </body>
     </html>
     '''
