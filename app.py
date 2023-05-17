07f6d03d35ecb72e66d0346dbaf2ad10dd3a4025
yin895902139 <40667536+yin895902139@users.noreply.github.com>
2023-05-17 16:07:36 +0000

    123

--- app.py
+++ app.py
@@ -7,55 +7,53 @@ start_time = datetime.datetime(2022, 5, 20)  # 设置起始时间
 
 @app.route('/')
 def hello_world():
-    return '''
+    return f'''
     <html>
     <head>
       <style>
         /* 定义动画效果 */
-        @keyframes text-animation {
-          0% { opacity: 0; }
-           50% { opacity: 1; }
-           100% { opacity: 0; }
-         }
-         
-         /* 应用动画到文字元素 */
-         .animated-text {
-           animation: text-animation 2s infinite;
-         }
-         
-         /* 设置桃心字符的文本颜色为红色 */
-         .heart {
-           color: red;
-         }
-       </style>
-       <script>
-         var days; // 声明全局变量
+        @keyframes text-animation {{
+          0% {{ opacity: 0; }}
+          50% {{ opacity: 1; }}
+          100% {{ opacity: 0; }}
+        }}
+        
+        /* 应用动画到文字元素 */
+        .animated-text {{
+          animation: text-animation 2s infinite;
+        }}
+        
+        /* 设置桃心字符的文本颜色为红色 */
+        .heart {{
+          color: red;
+        }}
+      </style>
+      <script>
+        var days; // 声明全局变量
 
-         function updateTime() {
-           var current_time = new Date();
-           var start_time = new Date("{start_time}");
-           var time_difference = current_time - start_time;
+        function updateTime() {{
+          var current_time = new Date();
+          var start_time = new Date("{start_time}");
+          var time_difference = current_time - start_time;
 
-           days = Math.floor(time_difference / (1000 * 60 * 60 * 24));
-           var hours = Math.floor((time_difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-           var minutes = Math.floor((time_difference % (1000 * 60 * 60)) / (1000 * 60));
-           var seconds = Math.floor((time_difference % (1000 * 60)) / 1000);
+          days = Math.floor(time_difference / (1000 * 60 * 60 * 24));
+          var minutes = Math.floor((time_difference % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((time_difference % (1000 * 60)) / 1000);
 
-           var element = document.getElementById("time-together");
-           var timeString = days + "天 " + hours + "小时 " + minutes + "分钟 " + seconds + "秒";
-           element.innerHTML = "我们在一起已经 " + timeString;
-         }
-         
-         window.setInterval(updateTime, 1000); // 每秒更新一次时间
-       </script>
-     </head>
-     <body onload="updateTime()"> <!-- 使用 onload 事件调用 updateTime 函数 -->
-       <h1 class="animated-text">我爱你<span class="heart">❤</span>，小莹！</h1>
-       <p id="time-together"></p>
-       <img src="https://c-ssl.duitang.com/uploads/blog/202209/22/20220922161509_bc733.jpeg" alt="图片描述">
-     </body>
-     </html>
-     '''
+          var element = document.getElementById("time-together");
+          var timeString = days + "天 " + hours + "小时 " + minutes + "分钟 " + seconds + "秒";
+          element.innerHTML = "我们在一起已经 " + timeString;
+        }}
+        
+        window.setInterval(updateTime, 1000); // 每秒更新一次时间
+      </script>
+    </head>
+    <body onload="updateTime()"> <!-- 使用 onload 事件调用 updateTime 函数 -->
+      <h1 class="animated-text">我爱你<span class="heart">❤</span>，小莹！</h1>
+      <p id="time-together"></p>
+    </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run()
